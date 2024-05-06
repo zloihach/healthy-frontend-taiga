b4c39e7aab6f748545eeb2f728edbef22c29e449
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-06 16:35:08 +0300

    🥵add children page

--- src/app/pages/children/children.component.ts
+++ src/app/pages/children/children.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ChildCardComponent} from "../../shared/components/child-card/child-card.component";
 
 @Component({
   selector: 'app-children',
   standalone: true,
-  imports: [],
+  imports: [
+    ChildCardComponent
+  ],
   templateUrl: './children.component.html',
   styleUrl: './children.component.less'
 })
