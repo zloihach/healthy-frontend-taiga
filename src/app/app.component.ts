fbdaa5a125cd0e6f6fff2783f32eb5eeaf282563
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-11 17:49:22 +0300

    ✅edit signup.component.less

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {HeaderComponent} from "./layout/header/header.component";
-import {SidebarComponent} from "./layout/sidebar/sidebar.component";
 import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule,HttpClientModule, TuiDialogModule, TuiAlertModule, LoginComponent, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, TuiRootModule,HttpClientModule, TuiDialogModule, TuiAlertModule, LoginComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
     providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
