10bb45e853a19dffd0e7cfcaf22e305d7fc884a8
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-16 13:51:18 +0300

    📝work with auth !!!!!

--- src/app/pages/calendar/vaccine.service.ts
+++ src/app/pages/calendar/vaccine.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Vaccine } from '../../shared/interfaces/vaccine.interface';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Vaccine} from '../../shared/interfaces/vaccine.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,28 @@ import { Vaccine } from '../../shared/interfaces/vaccine.interface';
 export class VaccineService {
   private apiUrl = 'http://localhost:3010/vaccination/user-vaccinations';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
+
+  private getCookie(name: string): string | null {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    console.log(`Getting cookie ${name}:`, match ? decodeURIComponent(match[2]) : null);
+    return match ? decodeURIComponent(match[2]) : null;
+  }
 
   getAllVaccinationsForCurrentUser(): Observable<Vaccine[]> {
     console.log('Sending request to get vaccinations'); // Log before sending request
-    return this.http.get<Vaccine[]>(this.apiUrl);
+
+    const authToken = this.getCookie('access-token');
+    if (!authToken) {
+      console.error('Auth token not found in cookies');
+    }
+
+    // Добавляем токен в заголовок Cookie
+    const headers = new HttpHeaders({
+      'Cookie': `access-token=${authToken}`
+    });
+
+    return this.http.post<Vaccine[]>(this.apiUrl, {headers}, {withCredentials: true});
   }
 }
