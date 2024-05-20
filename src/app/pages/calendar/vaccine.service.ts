114d7694a88576e2e6dea878aa3ae26d722aa2d9
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-20 17:08:01 +0300

    ✅edit requests

--- src/app/pages/calendar/vaccine.service.ts
+++ src/app/pages/calendar/vaccine.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs";
 import {Injectable} from "@angular/core";
 
@@ -7,18 +7,15 @@ import {Injectable} from "@angular/core";
 })
 export class VaccineService {
   private userApiUrl = 'http://localhost:3010/vaccination/user-vaccinations';
-  private childrenApiUrl = 'http://localhost:3010/children/all';
+  private childrenApiUrl = 'http://localhost:3010/children';
 
   constructor(private http: HttpClient) {}
 
   getUserVaccinations(): Observable<any> {
-    console.log('VaccineService: getUserVaccinations called');
-
-    return this.http.post<any>(this.userApiUrl,{}, { withCredentials: true });
+    return this.http.get<any>(this.userApiUrl, { withCredentials: true });
   }
 
   getChildren(): Observable<any[]> {
-    console.log('VaccineService: getChildren called');
-    return this.http.post<any[]>(this.childrenApiUrl,{}, { withCredentials: true });
+    return this.http.get<any[]>(this.childrenApiUrl,{withCredentials: true});
   }
 }
