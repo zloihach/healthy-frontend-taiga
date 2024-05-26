8d6d5072166f9f1ab1c10ebaa261010b9b526c53
zloihach <72695485+zloihach@users.noreply.github.com>
2024-05-26 03:02:51 +0300

    ✅add isLoading indicator for vaccine card in calendar component

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,202 +1,3 @@
-// // import { ChangeDetectorRef, Component, Input, OnInit, AfterViewChecked } from '@angular/core';
-// // import { Store } from '@ngrx/store';
-// // import { Vaccine } from '../../shared/interfaces/vaccine.interface';
-// // import { VaccineType } from '../../shared/enums/vaccine-type.enum';
-// // import {Observable, of} from 'rxjs';
-// // import { selectUserVaccinations, selectChildrenVaccinations } from '../../shared/states/selectors/main.selectors';
-// // import * as VaccineAction from '../../shared/states/actions/main.actions';
-// // import { VaccineCardComponent } from "../../shared/components/cards/vaccine-card/vaccine-card.component";
-// // import { NgClass, NgForOf, NgIf } from "@angular/common";
-// // import { VaccineTypePickerComponent } from "../../shared/components/pickers/vaccine-type-picker/vaccine-type-picker.component";
-// // import { UserPickerTabsComponent } from "../../shared/components/pickers/user-picker-tabs/user-picker-tabs.component";
-// // import { AppStateInterface } from "../../shared/interfaces/appStates.interface";
-// // import { ActivatedRoute } from '@angular/router';
-// // import {map, switchMap, tap} from "rxjs/operators";
-// // import {VaccineFilterComponent} from "../../shared/components/filters/vaccine-filter/vaccine-filter.component";
-// //
-// // @Component({
-// //   selector: 'app-calendar',
-// //   templateUrl: './calendar.component.html',
-// //   standalone: true,
-// //   imports: [
-// //     VaccineCardComponent,
-// //     NgForOf,
-// //     VaccineTypePickerComponent,
-// //     UserPickerTabsComponent,
-// //     NgIf,
-// //     NgClass,
-// //     VaccineFilterComponent
-// //   ],
-// //   styleUrls: ['./calendar.component.less']
-// // })
-// // export class CalendarComponent implements OnInit, AfterViewChecked {
-// //   @Input() vaccinations: Vaccine[] = [];
-// //   filteredVaccines: Vaccine[] = [];
-// //   selectedVaccineType: VaccineType = VaccineType.CALENDAR;
-// //   selectedUser: any = { id: 'user' };
-// //   userVaccinations$: Observable<Vaccine[]>;
-// //   childrenVaccinations$: Observable<Vaccine[] | undefined>;
-// //   userId: number | undefined;
-// //
-// //   constructor(private store: Store<AppStateInterface>, private cdr: ChangeDetectorRef, private route: ActivatedRoute) {
-// //     this.userVaccinations$ = this.store.select(selectUserVaccinations);
-// //     this.childrenVaccinations$ = this.route.params.pipe(
-// //       map(params => params['id']),
-// //       tap(id => this.userId = id ? +id : undefined),
-// //       switchMap(id => id ? this.store.select(selectChildrenVaccinations(id)) : of(undefined))
-// //     );
-// //   }
-// //
-// //   ngOnInit(): void {
-// //     if (this.userId) {
-// //       this.childrenVaccinations$.subscribe(vaccinations => {
-// //         this.vaccinations = vaccinations || [];
-// //         this.filterVaccines();
-// //         this.cdr.detectChanges();
-// //       });
-// //     } else {
-// //       this.store.dispatch(VaccineAction.loadUserVaccinations());
-// //       this.userVaccinations$.subscribe(vaccinations => {
-// //         this.vaccinations = vaccinations;
-// //         this.filterVaccines();
-// //         this.cdr.detectChanges();
-// //       });
-// //     }
-// //   }
-// //
-// //   ngAfterViewChecked(): void {
-// //     this.cdr.detectChanges();
-// //   }
-// //
-// //   onUserChange({ user, vaccinations }: { user: any, vaccinations: any[] }): void {
-// //     this.selectedUser = user;
-// //     this.vaccinations = vaccinations;
-// //     this.filterVaccines();
-// //     this.cdr.detectChanges();
-// //   }
-// //
-// //   onVaccineTypeChange(type: VaccineType): void {
-// //     this.selectedVaccineType = type;
-// //     this.filterVaccines();
-// //     this.cdr.detectChanges();
-// //   }
-// //
-// //   filterVaccines(): void {
-// //     if (!this.vaccinations) {
-// //       console.error('filterVaccines: vaccinations is null');
-// //       return;
-// //     }
-// //
-// //     this.filteredVaccines = this.vaccinations.filter(vaccine =>
-// //       this.selectedVaccineType === VaccineType.CALENDAR
-// //         ? vaccine.vaccine.type === VaccineType.CALENDAR
-// //         : vaccine.vaccine.type === VaccineType.EPIDEMIOLOGY
-// //     );
-// //     this.cdr.detectChanges();
-// //   }
-// // }
-//
-//
-// import { ChangeDetectorRef, Component, Input, OnInit, AfterViewChecked } from '@angular/core';
-// import { Store } from '@ngrx/store';
-// import { Vaccine } from '../../shared/interfaces/vaccine.interface';
-// import { VaccineType } from '../../shared/enums/vaccine-type.enum';
-// import { Observable, of } from 'rxjs';
-// import { selectUserVaccinations, selectChildrenVaccinations } from '../../shared/states/selectors/main.selectors';
-// import * as VaccineAction from '../../shared/states/actions/main.actions';
-// import { VaccineCardComponent } from "../../shared/components/cards/vaccine-card/vaccine-card.component";
-// import { NgClass, NgForOf, NgIf } from "@angular/common";
-// import { VaccineTypePickerComponent } from "../../shared/components/pickers/vaccine-type-picker/vaccine-type-picker.component";
-// import { UserPickerTabsComponent } from "../../shared/components/pickers/user-picker-tabs/user-picker-tabs.component";
-// import { AppStateInterface } from "../../shared/interfaces/appStates.interface";
-// import { ActivatedRoute } from '@angular/router';
-// import { map, switchMap, tap } from "rxjs/operators";
-// import { VaccineFilterComponent } from "../../shared/components/filters/vaccine-filter/vaccine-filter.component";
-//
-// @Component({
-//   selector: 'app-calendar',
-//   templateUrl: './calendar.component.html',
-//   standalone: true,
-//   imports: [
-//     VaccineCardComponent,
-//     NgForOf,
-//     VaccineTypePickerComponent,
-//     UserPickerTabsComponent,
-//     NgIf,
-//     NgClass,
-//     VaccineFilterComponent
-//   ],
-//   styleUrls: ['./calendar.component.less']
-// })
-// export class CalendarComponent implements OnInit, AfterViewChecked {
-//   @Input() vaccinations: Vaccine[] = [];
-//   filteredVaccines: Vaccine[] = [];
-//   selectedVaccineType: VaccineType = VaccineType.ALL;
-//   selectedUser: any = { id: 'user' };
-//   userVaccinations$: Observable<Vaccine[]>;
-//   childrenVaccinations$: Observable<Vaccine[] | undefined>;
-//   userId: number | undefined;
-//
-//   constructor(private store: Store<AppStateInterface>, private cdr: ChangeDetectorRef, private route: ActivatedRoute) {
-//     this.userVaccinations$ = this.store.select(selectUserVaccinations);
-//     this.childrenVaccinations$ = this.route.params.pipe(
-//       map(params => params['id']),
-//       tap(id => this.userId = id ? +id : undefined),
-//       switchMap(id => id ? this.store.select(selectChildrenVaccinations(id)) : of(undefined))
-//     );
-//   }
-//
-//   ngOnInit(): void {
-//     if (this.userId) {
-//       this.childrenVaccinations$.subscribe(vaccinations => {
-//         this.vaccinations = vaccinations || [];
-//         this.filterVaccines();
-//         this.cdr.detectChanges();
-//       });
-//     } else {
-//       this.store.dispatch(VaccineAction.loadUserVaccinations());
-//       this.userVaccinations$.subscribe(vaccinations => {
-//         this.vaccinations = vaccinations;
-//         this.filterVaccines();
-//         this.cdr.detectChanges();
-//       });
-//     }
-//   }
-//
-//   ngAfterViewChecked(): void {
-//     this.cdr.detectChanges();
-//   }
-//
-//   onUserChange({ user, vaccinations }: { user: any, vaccinations: any[] }): void {
-//     this.selectedUser = user;
-//     this.vaccinations = vaccinations;
-//     this.filterVaccines();
-//     this.cdr.detectChanges();
-//   }
-//
-//   onVaccineTypeChange(type: VaccineType): void {
-//     this.selectedVaccineType = type;
-//     this.filterVaccines();
-//     this.cdr.detectChanges();
-//   }
-//
-//   filterVaccines(): void {
-//     if (!this.vaccinations) {
-//       console.error('filterVaccines: vaccinations is null');
-//       return;
-//     }
-//
-//     if (this.selectedVaccineType === VaccineType.ALL) {
-//       this.filteredVaccines = this.vaccinations;
-//     } else {
-//       this.filteredVaccines = this.vaccinations.filter(vaccine =>
-//         vaccine.vaccine.type === this.selectedVaccineType
-//       );
-//     }
-//     this.cdr.detectChanges();
-//   }
-// }
-//
 // import { ChangeDetectorRef, Component, Input, OnInit, AfterViewChecked } from '@angular/core';
 // import { Store } from '@ngrx/store';
 // import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -213,8 +14,8 @@
 // import { VaccineFilterComponent } from "../../shared/components/filters/vaccine-filter/vaccine-filter.component";
 // import { Vaccine } from '../../shared/interfaces/vaccine.interface';
 // import { VaccineType } from '../../shared/enums/vaccine-type.enum';
-// import {VaccinationStatus} from "../../shared/enums/vaccination-status.types";
-// import {TuiFilterModule} from "@taiga-ui/kit";
+// import { VaccinationStatus } from '../../shared/enums/vaccination-status.types';
+// import { TuiFilterModule } from "@taiga-ui/kit";
 //
 // @Component({
 //   selector: 'app-calendar',
@@ -304,16 +105,20 @@
 //     const filters = this.form.get('filters')!.value;
 //     const now = new Date();
 //
-//     // If no filters are selected, show all vaccinations
-//     if (filters.length === 0) {
-//       this.filteredVaccines = this.vaccinations;
-//       this.cdr.detectChanges();
-//       return;
-//     }
-//
 //     this.filteredVaccines = this.vaccinations.filter(vaccine => {
 //       const plannedVaccinationDate = vaccine.planned_vaccination_date ? new Date(vaccine.planned_vaccination_date) : null;
 //
+//       // Filter by vaccine type
+//       if (this.selectedVaccineType !== VaccineType.ALL && vaccine.vaccine.type !== this.selectedVaccineType) {
+//         return false;
+//       }
+//
+//       // If no filters are selected, show all vaccinations
+//       if (filters.length === 0) {
+//         return true;
+//       }
+//
+//       // Apply status filters
 //       if (filters.includes(VaccinationStatus.DONE) && vaccine.is_vaccinated) {
 //         return true;
 //       }
@@ -332,11 +137,13 @@
 //     this.cdr.detectChanges();
 //   }
 // }
+
+
 import { ChangeDetectorRef, Component, Input, OnInit, AfterViewChecked } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Observable, of } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { map, switchMap, tap, debounceTime } from 'rxjs/operators';
 import { selectUserVaccinations, selectChildrenVaccinations } from '../../shared/states/selectors/main.selectors';
 import * as VaccineAction from '../../shared/states/actions/main.actions';
 import { VaccineCardComponent } from "../../shared/components/cards/vaccine-card/vaccine-card.component";
@@ -376,8 +183,8 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
   userVaccinations$: Observable<Vaccine[]>;
   childrenVaccinations$: Observable<Vaccine[] | undefined>;
   userId: number | undefined;
+  isLoading = true; // Add a loading indicator
 
-  // Initialize form group
   form: FormGroup;
   items: string[] = Object.values(VaccinationStatus);
   disabledItemHandler: (item: string) => boolean = (item: string) => item.length < 7;
@@ -390,7 +197,6 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
       switchMap(id => id ? this.store.select(selectChildrenVaccinations(id)) : of(undefined))
     );
 
-    // Initialize form
     this.form = new FormGroup({
       filters: new FormControl([]),
     });
@@ -401,6 +207,7 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
       this.childrenVaccinations$.subscribe(vaccinations => {
         this.vaccinations = vaccinations || [];
         this.filterVaccines();
+        this.isLoading = false;
         this.cdr.detectChanges();
       });
     } else {
@@ -408,12 +215,12 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
       this.userVaccinations$.subscribe(vaccinations => {
         this.vaccinations = vaccinations;
         this.filterVaccines();
+        this.isLoading = false;
         this.cdr.detectChanges();
       });
     }
 
-    // React to form changes
-    this.form.get('filters')!.valueChanges.subscribe(() => {
+    this.form.get('filters')!.valueChanges.pipe(debounceTime(300)).subscribe(() => {
       this.filterVaccines();
     });
   }
@@ -442,12 +249,10 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
     this.filteredVaccines = this.vaccinations.filter(vaccine => {
       const plannedVaccinationDate = vaccine.planned_vaccination_date ? new Date(vaccine.planned_vaccination_date) : null;
 
-      // Filter by vaccine type
       if (this.selectedVaccineType !== VaccineType.ALL && vaccine.vaccine.type !== this.selectedVaccineType) {
         return false;
       }
 
-      // If no filters are selected, show all vaccinations
       if (filters.length === 0) {
         return true;
       }
@@ -469,5 +274,9 @@ export class CalendarComponent implements OnInit, AfterViewChecked {
     });
 
     this.cdr.detectChanges();
+  }
+
+  trackByVaccineId(index: number, vaccine: Vaccine): number {
+    return vaccine.id;
   }
 }
